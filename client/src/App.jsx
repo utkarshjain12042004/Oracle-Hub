b2bbcaf8d7c545d43dc6288a5f741e5c5039c119
Utkarsh <utkarshjain12042004@gmail.com>
2023-12-16 12:24:13 +0000

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Reports from "./pages/Reports"
+import About from "./pages/About"
+import ReusableComponents from "./pages/ReusableComponents"
+import ContactUs from "./pages/ContactUs"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/reusable-components" element={<ReusableComponents />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/ContactUs.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+export default function ContactUs() {
+  return (
+    <div>Contact Us</div>
+  )
+}
